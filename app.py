9531655a096bdd3a8546b58d959298ef8c6cd49b
Anthony Solorzano Lopez <anthony06891@gmail.com>
2025-02-13 23:17:19 -0600

    testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "¡Hola desde Cloud Run con Python 3!", 200
+    return "Hello from Cloud Run with Python 3!", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
