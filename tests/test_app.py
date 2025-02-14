3f7e9ef52184eeb0c3aa0fa64084b5e51702c10d
Anthony Solorzano Lopez <anthony06891@gmail.com>
2025-02-13 22:52:12 -0600

    testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ from app import app
 
 @pytest.fixture
 def client():
-    """Configuración del cliente de pruebas para Flask."""
+    """Configura el cliente de pruebas para Flask."""
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
@@ -11,5 +11,5 @@ def client():
 def test_home(client):
     """Prueba que la ruta raíz '/' responde correctamente."""
     response = client.get("/")
-    assert response.status_code == 200
-    assert response.data.decode("utf-8") == "¡Hola desde Cloud Run con Python 3!"
+    pytest.assume(response.status_code == 200)
+    pytest.assume(response.data.decode("utf-8") == "¡Hola desde Cloud Run con Python 3!")
